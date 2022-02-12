9521836d80e85e4495416c81f717ce9d8527de37
Vladislav Kotsur <vladiskor@gmail.com>
2022-02-12 18:59:40 +0300

    Added first task for Lesson 2

---
+++ src/main/java/ru/vladkots/se/Lesson2/Lesson2.java
@@ -0,0 +1,15 @@
+package ru.vladkots.se.Lesson2;
+
+public class Lesson2 {
+    public static void main(String[] args) {
+        int[] arr = {1, 0, 0, 0, 1, 1, 0, 1, 1, 0};
+        for (int i = 0; i < arr.length; i++){
+            if (arr[i] == 0) {
+                arr[i] = 1;
+            } else {
+                arr[i] = 0;
+            }
+            System.out.print(arr[i] + " ");
+        }
+    }
+}
