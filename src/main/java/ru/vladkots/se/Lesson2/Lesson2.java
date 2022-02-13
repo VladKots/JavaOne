b24fa6a9d7df33db215b79bc798741aa4367e307
Vladislav Kotsur <vladiskor@gmail.com>
2022-02-13 19:45:11 +0300

    Added 2nd & 3rd task for Lesson 2

--- src/main/java/ru/vladkots/se/Lesson2/Lesson2.java
+++ src/main/java/ru/vladkots/se/Lesson2/Lesson2.java
@@ -2,8 +2,9 @@ package ru.vladkots.se.Lesson2;
 
 public class Lesson2 {
     public static void main(String[] args) {
+        // task 1
         int[] arr = {1, 0, 0, 0, 1, 1, 0, 1, 1, 0};
-        for (int i = 0; i < arr.length; i++){
+        for (int i = 0; i < arr.length; i++) {
             if (arr[i] == 0) {
                 arr[i] = 1;
             } else {
@@ -11,5 +12,31 @@ public class Lesson2 {
             }
             System.out.print(arr[i] + " ");
         }
+        System.out.println();
+        //task 2
+        fillArr();
+        System.out.println();
+        //task 3
+        changeArr();
+    }
+
+    public static void fillArr() {
+        int[] arr = new int[8];
+        arr[0] = 0;
+        System.out.print(arr[0] + " ");
+        for (int i = 1; i < arr.length; i++) {
+            arr[i] = arr[i - 1] + 3;
+            System.out.print(arr[i] + " ");
+        }
+    }
+
+    public static void changeArr() {
+        int[] charr = {1, 5, 3, 2, 11, 4, 5, 2, 4, 8, 9, 1};
+        for (int i = 0; i < charr.length; i++) {
+            if (charr[i] < 6) charr[i] = charr[i] * 2;
+            System.out.print(charr[i] + " ");
+        }
     }
 }
+
+
