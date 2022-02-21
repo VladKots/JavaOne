746dd5dcc707cd61c95bbf087ccf44dc77ea3b4a
Vladislav Kotsur <vladiskor@gmail.com>
2022-02-21 21:38:55 +0300

    Added 7th task. Lesson 2 is ready.

--- src/main/java/ru/vladkots/se/Lesson2/Lesson2.java
+++ src/main/java/ru/vladkots/se/Lesson2/Lesson2.java
@@ -1,5 +1,7 @@
 package ru.vladkots.se.Lesson2;
 
+import java.util.Arrays;
+
 public class Lesson2 {
     public static void main(String[] args) {
         // task 1
@@ -31,6 +33,13 @@ public class Lesson2 {
         System.out.println(checkBalance(arr63));
         int[] arr64 = {20, 20};
         System.out.println(checkBalance(arr64));
+
+        int[] arr71 = {1, 2, 3, 4, 5};
+        movePosition(arr71, 2);
+        System.out.println(Arrays.toString(arr71));
+        int[] arr72 = {1, 2, 3, 4, 5};
+        movePosition(arr72, -2);
+        System.out.println(Arrays.toString(arr72));
     }
 
     public static void fillArr() {
@@ -82,20 +91,47 @@ public class Lesson2 {
 
     public static boolean checkBalance(int[] arr) {
         int sum = 0;
+        //ищем сумму элементов
         for (int i = 0; i < arr.length; i++) {
             sum = sum + arr[i];
         }
         if ((sum % 2) == 0) {
+            //если сумма делится на 2 - продолжаем поиски
             int a = 0;
             sum = sum / 2;
             int i = 0;
             while (i < arr.length && a <= sum) {
+                //высчитываем сумму левой части
                 a = a + arr[i];
                 if (a == sum) return true;
                 i++;
             }
         }
         return false;
+    }
+
+    public static int[] movePosition(int[] arr, int n) {
+        if (n > 0) {
+            //двигаем вправо
+            for (int i = 1; i <= n; i++) {
+                int tmp = arr[(arr.length - 1)];
+                for (int j = (arr.length - 1); j > 0; j--) {
+                    arr[j] = arr[(j - 1)];
+                }
+                arr[0] = tmp;
+            }
+        } else {
+            //двигаем влево
+            n = Math.abs(n);
+            for (int i = 1; i <= n; i++) {
+                int tmp = arr[0];
+                for (int j = 1; j < arr.length; j++) {
+                    arr[(j - 1)] = arr[j];
+                }
+                arr[(arr.length - 1)] = tmp;
+            }
+        }
+        return arr;
     }
 }
 
